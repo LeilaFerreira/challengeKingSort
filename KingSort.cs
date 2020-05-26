6b68358f6dbda473c9bccc7d837bf1ff67c0c78d
LeilaFerreira <leilaferreira.bhte@gmail.com>
2020-05-26 17:17:48 -0300

    jump

---
+++ KingSort.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestEnciclopédia
+{
+    class KingSort
+    {
+
+        private static readonly string[] romanos = { "I","V","X","L","C" };
+
+
+        public String[] getSortedList(String[] reis) {
+
+            var arryReis = new kings[reis.Length];
+            String[] reisOrdenados = new string[reis.Length];
+
+
+         for (int i = 0; i < reis.Length; i++)
+            {
+
+                string[] rei = reis[i].Split(' ');
+                arryReis[i] = new kings();
+
+                arryReis[i].nomeRei =  rei[0];
+                arryReis[i].numeroRonamo = rei[1];
+                arryReis[i].numeroDecimal = romanoParaDecimal(arryReis[i].numeroRonamo);
+
+            }
+            
+            var listaNOmeOrdenada = arryReis.OrderBy(a => a.nomeRei).ThenBy(a => a.numeroDecimal);
+
+            List<kings> lista = new List<kings>(listaNOmeOrdenada);
+
+            var indice = 0;
+            lista.ForEach(delegate (kings x)
+               {
+                   reisOrdenados[indice] = x.nomeRei+ "  "+x.numeroRonamo;
+                   indice++;
+                   
+              });
+
+           
+       
+            return reisOrdenados;
+
+        }
+
+        private int romanoParaDecimal(string numeroRomano)
+        {
+            int retorno = 0;
+
+            if (numeroRomano.Length == 1)
+            {
+                retorno = dictionaryRomano(numeroRomano.ToUpper());
+            }
+            else
+            {
+                                
+                int indiceArray = numeroRomano.Length ;
+                int[] decimais = new int[indiceArray];
+
+                for (int i =0; i < numeroRomano.Length; i++)
+                {
+                    decimais[i] = dictionaryRomano(numeroRomano[i].ToString());
+                }
+
+               
+                int total = 0;
+                for (int i = 0; i < decimais.Length; i++)
+                {
+                        if (decimais.Length % 2 == 0) {
+
+                                try
+                                {
+                                    if (decimais[i] >= decimais[i + 1])
+                                    {
+                                        total += decimais[i] + decimais[i + 1];
+                                    }
+                                    else if (decimais[i] < decimais[i + 1])
+                                    {
+                                        total += decimais[i + 1] - decimais[i];
+                                    }
+
+                                    i++;
+                                }catch(Exception e) { }
+                        }
+                        else
+                        {
+                                try
+                                {
+                                    if (decimais[i] >= decimais[i + 1])
+                                    {
+                                        total += decimais[i];
+                                    }
+                                    else if (decimais[i] < decimais[i + 1])
+                                    {
+                                        total += decimais[i + 1] - decimais[i];
+                                    }
+                                }catch (Exception e)
+                                {
+                                    total += decimais[i];
+                                 }
+
+                        }
+                }
+
+                retorno = total;
+
+            }
+            
+            return retorno;
+        }
+
+
+
+        private int dictionaryRomano(string numeroRomano)
+        {
+            Dictionary<String, int> hash = new Dictionary<string, int>();
+            hash.Add("I", 1);
+            hash.Add("V", 5);
+            hash.Add("X", 10);
+            hash.Add("L", 50);
+            hash.Add("C", 100);
+          
+
+            return hash[numeroRomano];
+        }
+    }
+
+
+    class kings
+    {
+        public string nomeRei { get; set; }
+        public string numeroRonamo { get; set; }
+        public int numeroDecimal { get; set; }
+     
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestEnciclopédia
+{
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            KingSort kingSort = new KingSort();
+
+                       string[] kings = new String[17];
+
+                       kings[0] = "Louis IX";
+                       kings[1] = "Louis VIII";
+                       kings[2] = "Philippe II";
+                       kings[3] = "Richard I";
+                       kings[4] = "Richard II";
+                       kings[5] = "Richard III";
+                       kings[6] = "João X";
+                       kings[7] = "João I";
+                       kings[8] = "João L";
+                       kings[9] = "João V";
+                       kings[10] = "Philippe VI";
+                       kings[11] = "Jean II";
+                       kings[12] = "Charles V";
+                       kings[13] = "Charles VII";
+                       kings[14] = "Charles VI";
+                       kings[15] = "Louis XI";
+                       kings[16] = "Philip II";
+
+           String[] reisOrdenados =   kingSort.getSortedList(kings);
+        
+           for(int i = 0; i < reisOrdenados.Length; i++)
+           {
+                 Console.WriteLine(reisOrdenados[i]);
+           };
+
+            Console.WriteLine("Pressione ENTER para sair...");
+            Console.Read();
+              
+        }
+
+
+    }
+}
